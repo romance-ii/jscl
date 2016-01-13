80be8924700bb84179b737bbd6f76d9c118a3195
David Vazquez <davazp@gmail.com>
2016-01-14 00:12:40 +0100

    Emit accepts T to declare variable automatically

--- src/compiler/compiler.lisp
+++ src/compiler/compiler.lisp
@@ -69,6 +69,9 @@
 ;;; VAR. VAR is returned.
 ;;; 
 (defun emit (expr &optional var (target *target*))
+  (when (eq var t)
+    (setq var (gvarname))
+    (emit `(var ,var)))
   (let ((stmt (if var `(= ,var ,expr) expr)))
     (push-to-target stmt target)
     var))
@@ -1641,9 +1644,6 @@
 ;;; Like `convert', but returns a symbol and emit the result of the
 ;;; compilation.
 (defun convert* (sexp &optional out multiple-value-p)
-  (when (eq out t)
-    (setq out (gvarname))
-    (emit `(var ,out)))
   (let ((res (convert sexp multiple-value-p)))
     (emit res out)))
 
