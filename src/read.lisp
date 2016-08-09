b0f0a3b8f6785255ec4b253e481263459c664257
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-08-09 15:50:25 -0400

    Read #\U+xxxx char literals

--- src/read.lisp
+++ src/read.lisp
@@ -260,7 +260,7 @@
       (#\\
        (cond ((and (char-equal #\U (%peek-char stream))
                    (char-equal #\+ (%peek-char stream 1)))
-              (%read-char stream)       ; U (or u)
+              (%read-char stream) ; U (or u)
               (%read-char stream) ; +
               (let ((hex-id (read-until stream (lambda (ch)
                                                  (not (digit-char-p ch 16))))))
