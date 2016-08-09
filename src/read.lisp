019f361d8966d430d5913aa8f44cd81d17664e46
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-08-09 16:57:25 -0400

    Fix READer for binary, octal, hex.
    This includes patching PARSE-INTEGER and adds #b #o #x faux-reader-macros

--- src/read.lisp
+++ src/read.lisp
@@ -259,15 +259,15 @@
           (read-escaped-until stream #'terminalp)))))
       (#\\
        (cond ((and (char-equal #\U (%peek-char stream))
-                (char-equal #\+ (%peek-char stream 1)))
+                   (char-equal #\+ (%peek-char stream 1)))
               (%read-char stream)       ; U (or u)
-              (%read-char stream)       ; +
+              (%read-char stream) ; +
               (let ((hex-id (read-until stream (lambda (ch)
-                                              (not (digit-char-p ch 16))))))
+                                                 (not (digit-char-p ch 16))))))
                 (code-char (parse-integer hex-id :radix 16))))
              (t (let ((cname
-              (concat (string (%read-char stream))
-                      (read-until stream #'terminalp))))
+                       (concat (string (%read-char stream))
+                               (read-until stream #'terminalp))))
                   (let ((ch (name-char cname)))
                     (or ch (char cname 0)))))))
       ((#\+ #\-)
