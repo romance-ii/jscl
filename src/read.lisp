969834c3ad0a002716fd0e33b58fa7c16e8f7f54
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-08-09 17:49:37 -0400

    READer (pseudo-)macro #nR radix

--- src/read.lisp
+++ src/read.lisp
@@ -260,14 +260,14 @@
       (#\\
        (cond ((and (char-equal #\U (%peek-char stream))
                    (char-equal #\+ (%peek-char stream 1)))
-              (%read-char stream) ; U (or u)
+           (%read-char stream) ; U (or u)
            (%read-char stream)          ; +
            (let ((hex-id (read-until stream (lambda (ch)
                                               (not (digit-char-p ch 16))))))
                 (code-char (parse-integer hex-id :radix 16))))
              (t (let ((cname
-              (concat (string (%read-char stream))
-                      (read-until stream #'terminalp))))
+                       (concat (string (%read-char stream))
+                               (read-until stream #'terminalp))))
                   (let ((ch (name-char cname)))
                     (or ch (char cname 0)))))))
       ((#\+ #\-)
