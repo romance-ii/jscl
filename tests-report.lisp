c76bfb65232a94284057004a2828a19c2a82f209
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2016-08-10 01:20:53 -0400

    report % expected to have passed

--- tests-report.lisp
+++ tests-report.lisp
@@ -4,7 +4,11 @@
 
  (if (= *passed-tests* *total-tests*)
      (format t "All the tests (~a) passed successfully.~%" *total-tests*)
-     (format t "~a/~a test(s) passed successfully.~%" *passed-tests* *total-tests*))
+     (format t "~a/~a test(s) passed successfully.~%" *passed-tests* *total-tests*)
+     (format t "(~d% of expected success)" (round *passed-tests*
+                                                  (- *total-tests*
+                                                     *expected-failures*
+                                                     *unexpected-passes*))))
 
  (unless (zerop *expected-failures*)
    (format t "~a test(s) failed expectedly.~%" *expected-failures*))
@@ -17,4 +21,4 @@
  #+jscl
  (when #j:phantom
    (#j:phantom:exit *failed-tests*))
-)
+ )
