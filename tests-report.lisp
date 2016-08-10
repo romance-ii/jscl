cfa53e5660e96f66a0b616310efb0f5f3528f4f1
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2016-08-10 01:25:22 -0400

    FIXUP: % of tests

--- tests-report.lisp
+++ tests-report.lisp
@@ -1,33 +1,34 @@
 (async
- (format t "~%Finished. The execution took ~a seconds.~%"
-         (/ (- (get-internal-real-time) *timestamp*) internal-time-units-per-second 1.0))
+  (format t "~%Finished. The execution took ~a seconds.~%"
+          (/ (- (get-internal-real-time) *timestamp*) internal-time-units-per-second 1.0))
 
- (if (= *passed-tests* *total-tests*)
-     (format t "All the tests (~a) passed successfully.~%" *total-tests*)
-     (format t "~a/~a test(s) passed successfully.~%" *passed-tests* *total-tests*)
-     (format t "(~d% of expected success)" (round *passed-tests*
-                                                  (- *total-tests*
-                                                     *expected-failures*
-                                                     *unexpected-passes*))))
- 
- (unless (zerop *expected-failures*)
-   (format t "~a test(s) failed expectedly.~%" *expected-failures*))
+  (if (= *passed-tests* *total-tests*)
+      (format t "All the tests (~a) passed successfully.~%" *total-tests*)
+      (format t "~a/~a test(s) passed successfully.~%(~d% of expected success)~%"
+              *passed-tests* *total-tests*
+              (round *passed-tests*
+                     (- *total-tests*
+                        *expected-failures*
+                        *unexpected-passes*))))
+  
+  (unless (zerop *expected-failures*)
+    (format t "~a test(s) failed expectedly.~%" *expected-failures*))
 
- (unless (zerop *unexpected-passes*)
-   (format t "~a test(s) passed unexpectedly.~%" *unexpected-passes*))
- 
- (let (unbound)
-   (do-external-symbols (symbol :jscl)
-     (unless (or (boundp symbol) (fboundp symbol))
-       (push symbol unbound)))
-   (when unbound 
-     (format t "Unbound, exported symbols in JSCL package:~%")
-     (dolist (symbol unbound)
-       (format t " • ~a~%" symbol))))
- 
- (terpri)
+  (unless (zerop *unexpected-passes*)
+    (format t "~a test(s) passed unexpectedly.~%" *unexpected-passes*))
+  
+  (let (unbound)
+    (do-external-symbols (symbol :jscl)
+      (unless (or (boundp symbol) (fboundp symbol))
+        (push symbol unbound)))
+    (when unbound 
+      (format t "Unbound, exported symbols in JSCL package:~%")
+      (dolist (symbol unbound)
+        (format t " • ~a~%" symbol))))
+  
+  (terpri)
 
- #+jscl
- (when #j:phantom
-   (#j:phantom:exit *failed-tests*))
- )
+  #+jscl
+  (when #j:phantom
+    (#j:phantom:exit *failed-tests*))
+  )
