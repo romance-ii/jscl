8f03a4e358ebc61a904e54bd416c77501a1a7843
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2016-08-11 02:04:50 -0400

    Emacs auto-indent misc.lisp

--- src/misc.lisp
+++ src/misc.lisp
@@ -1,17 +1,17 @@
 ;;; misc.lisp --
 
-;; JSCL is free software: you can redistribute it and/or
-;; modify it under the terms of the GNU General Public License as
-;; published by the Free Software Foundation, either version 3 of the
-;; License, or (at your option) any later version.
+;; JSCL is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General  Public License as published by the Free
+;; Software Foundation,  either version  3 of the  License, or  (at your
+;; option) any later version.
 ;;
-;; JSCL is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-;; General Public License for more details.
+;; JSCL is distributed  in the hope that it will  be useful, but WITHOUT
+;; ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+;; FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+;; for more details.
 ;;
-;; You should have received a copy of the GNU General Public License
-;; along with JSCL.  If not, see <http://www.gnu.org/licenses/>.
+;; You should  have received a  copy of  the GNU General  Public License
+;; along with JSCL. If not, see <http://www.gnu.org/licenses/>.
 
 (/debug "loading misc.lisp!")
 
@@ -29,7 +29,7 @@
 (defun long-site-name ()
   nil)
 
-;;; Javascript has not access to the hardware. Would it make sense to
+;;; Javascript has  not access to the  hardware. Would it make  sense to
 ;;; have the browser data as machine abstraction instead?
 
 (defun machine-instance ()
