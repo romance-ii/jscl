5b7b23a1d37eedfc224ffc536d49b589395a66a1
Bruce-Robert Fenn Pocock <brpocock@star-hope.org>
2016-08-11 15:38:22 -0400

    Report failing tests in a nice summary
    Summarizes  failing test  cases  and  whether they  had  been marked  as
    expecting to fail or not.

--- tests-report.lisp
+++ tests-report.lisp
@@ -1,39 +1,7 @@
 (async
-  (format t "~%Finished. The execution took ~a seconds.~%"
-          (/ (- (get-internal-real-time) *timestamp*) internal-time-units-per-second 1.0))
+ (format t "~%Finished. The execution took ~a seconds.~%"
+         (/ (- (get-internal-real-time) *timestamp*) internal-time-units-per-second 1.0))
 
-<<<<<<< d2eafe0d16620b22081789c992c7218750b240b9
-  (if (= *passed-tests* *total-tests*)
-      (format t "All the tests (~a) passed successfully.~%" *total-tests*)
-      (format t "~a/~a test(s) passed successfully.~%     (~d% of expected success)~%"
-              *passed-tests* *total-tests*
-              (floor (* 100 (/ *passed-tests*
-                               (- *total-tests*
-                                  *expected-failures*
-                                  *unexpected-passes*))))))
-  
-  (unless (zerop *expected-failures*)
-    (format t "~a test(s) failed expectedly.~%" *expected-failures*))
-
-  (unless (zerop *unexpected-passes*)
-    (format t "~a test(s) passed unexpectedly.~%" *unexpected-passes*))
-  
-  (let (unbound)
-    (do-external-symbols (symbol :jscl)
-      (unless (or (boundp symbol) (fboundp symbol))
-        (push symbol unbound)))
-    (when unbound 
-      (format t "Unbound, exported symbols in JSCL package:~%")
-      (dolist (symbol unbound)
-        (format t " • ~a~%" symbol))))
-  
-  (terpri)
-
-  #+jscl
-  (when #j:phantom
-    (#j:phantom:exit *failed-tests*))
-  )
-=======
  (if (= *passed-tests* *total-tests*)
      (format t "All the tests (~a) passed successfully.~%" *total-tests*)
      (format t "~a/~a test(s) passed successfully.~%     (~d% of expected success)~%"
@@ -50,18 +18,32 @@
    (format t "~a test(s) passed unexpectedly.~%" *unexpected-passes*))
 
  (let (unbound)
-   (do-external-symbols (symbol :jscl)
+    (dolist (package '(:cl :jscl/ffi :jscl/xhr))
+      (when (find-package package)
+        (do-external-symbols (symbol package)
      (unless (or (boundp symbol) (fboundp symbol))
        (push symbol unbound)))
    (when unbound
      (format t "Unbound, exported symbols in JSCL package:~%")
      (dolist (symbol unbound)
-       (format t " • ~a~%" symbol))))
+            (format t " • ~a~%" symbol))
+          (format t "~%~%")))))
+
+  (when *failed-tests-details*
+    (format t "~%~%Details of failing tests:~%")
+    (dolist (kind '(:failed-expected :failed))
+      (let ((results (remove-if #'null (mapcar (lambda (failure)
+                                                 (and (eql (second failure) kind)
+                                                      (first failure)))
+                                               *failed-tests-details*))))
+        (when results
+          (format t "~%~%Tests which ~a:~%" kind)
+          (dolist (failure results)
+            (format t "~% • ~s" failure)))))
+    (format t "~%~%"))
 
  (terpri)
 
  #+jscl
  (when #j:phantom
-   (#j:phantom:exit *failed-tests*))
- )
->>>>>>> Emacs auto-indent tests-report.lisp
+    (#j:phantom:exit *failed-tests*)))
