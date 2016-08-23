3be5458d00d9bb5f16cf6e94eae42b50d0ed1026
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2016-08-23 00:44:57 -0400

    use new JSCL/FFI for #j:

--- src/compat.lisp
+++ src/compat.lisp
@@ -41,7 +41,7 @@
                (position #\: descriptor :start start)))
          ((null end)
           (push (subseq descriptor start) subdescriptors)
-          `(oget *root* ,@(reverse subdescriptors)))
+          `(jscl/ffi::oget jscl/ffi::*root* ,@(reverse subdescriptors)))
       (push (subseq descriptor start end) subdescriptors))))
 
 (set-dispatch-macro-character #\# #\J #'j-reader)
