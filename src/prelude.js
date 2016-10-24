09ce5741a4494a134f09c601c958ff3005fd5116
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2016-10-23 22:47:38 -0400

    reformat

--- src/prelude.js
+++ src/prelude.js
@@ -247,7 +247,7 @@ internals.js_to_lisp = function (x) {
         return nil;
     else if (typeof x == 'function'){
         // Trampoline calling the JS function
-    var trampoline = function(values){
+        var trampoline = function(values){
             var args = Array.prototype.slice.call(arguments, 1);
             for (var i in args)
                 args[i] = internals.lisp_to_js(args[i]);
@@ -407,4 +407,3 @@ if (typeof module !== 'undefined' &&
     global.readline = require('readline');
 
 /* -- End of prelude -- */
-
