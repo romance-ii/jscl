f86dffebda363ddabc885599575bd865d721db2b
David Vazquez Pua <davazp@gmail.com>
2016-10-31 18:56:44 +0100

    Define dummy subtypep function

--- src/boot.lisp
+++ src/boot.lisp
@@ -394,6 +394,11 @@
          ,@clausules
          (t (error "~S fell through etypecase expression." ,g!x))))))
 
+
+;;; No type system is implemented yet.
+(defun subtypep (type1 type2)
+  (values nil nil))
+
 (defun notany (fn seq)
   (not (some fn seq)))
 
