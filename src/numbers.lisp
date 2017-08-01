cad0cd6dd70a380293d9e39dafca0868a00618f8
Bruce-Robert Fenn Pocock <brfennpocock@star-hope.org>
2017-08-01 17:46:45 -0400

    stray “git”

--- src/numbers.lisp
+++ src/numbers.lisp
@@ -1,4 +1,4 @@
-git ;;; numbers.lisp
+;;; numbers.lisp
 
 ;; JSCL is free software: you can redistribute it and/or modify it under
 ;; the terms of the GNU General  Public License as published by the Free
